4c379a9e0086ff1a52493109523cb3d9819c0540
Joshua Empson <j.empson@livelinktechnology.net>
2022-01-28 18:23:20 +0000

    Materialize installed, headers + footers, form

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 class Article < ApplicationRecord
   validates :title,       presence: true, length: { minimum: 6, maximum: 200 }
   validates :description, presence: true, length: { minimum: 10 }
+
+  def length_val(field, minmax)  ## pass field name and which value i.e. :title, :maximum)
+    _validators[field].select do |e|
+      e.is_a? ActiveRecord::Validations::LengthValidator
+    end.first.options[minmax]
+  end
 end
